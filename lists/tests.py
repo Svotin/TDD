105d5f782ace42021e72c01e87f7ad496e883761
Svotin <dogweb2022@Mail.ru>
2019-07-05 13:04:20 +0300

    Минимальное представление в HTML формате

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 class SmokeTest(TestCase):
     '''тест домашней страницы'''
@@ -8,3 +9,14 @@ class SmokeTest(TestCase):
         '''тест: корневой url преобразуется в представление домашней страницы'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''тест: домашняя страница возвращает правильный html'''
+        request = HttpRequest()
+        responce = home_page(request)
+        html = responce.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+
